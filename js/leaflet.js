895ae5ac2f99fa85cc785cf40160fc42f2d10ddf
mightymarabu <sebastian.n.schmidt@gmx.de>
2023-02-07 12:40:52 +0100

    add layers and content

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,6 +1,38 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([2.3357, 37.9573], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+
+var basemaps = {
+    OSM: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png"),
+    Google: L.tileLayer("http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    }),
+}
+//   geoserver
+var geoserver = 'http://141.51.249.91:8080/geoserver/marsabit/wms?'
+var overlay = { 
+    NDVI_20220930: L.tileLayer.wms(geoserver, {
+        layers: 'NDVI_20220930',
+        transparent: 'true',
+        format: 'image/png',
+    }),
+    what_is_here: L.tileLayer.wms(geoserver,{
+        layers: "whats_this",
+        transparent: 'true',
+        format: 'image/png',
+    }),
+}
+
+
+L.control.layers(basemaps, overlay).addTo(this.map);
+    
+basemaps.OSM.addTo(this.map);
+overlay.what_is_here.addTo(this.map);
+
+
+
+
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);

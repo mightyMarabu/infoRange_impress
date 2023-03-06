031c96d649a7eda23d1d3be937b66c2828a5c732
mightymarabu <sebastian.n.schmidt@gmx.de>
2023-03-06 09:48:27 +0100

    update content, rename index

--- js/leaflet.js
+++ js/leaflet.js
@@ -11,13 +11,23 @@ var basemaps = {
 //   geoserver
 var geoserver = 'http://141.51.249.91:8080/geoserver/marsabit/wms?'
 var overlay = { 
+    RGB_20230212: L.tileLayer.wms(geoserver, {
+        layers: 'rgb_230212',
+        transparent: 'true',
+        format: 'image/png',
+    }),
+    NDVI_20230212: L.tileLayer.wms(geoserver, {
+        layers: 'ndvi__ncc_230212',
+        transparent: 'true',
+        format: 'image/png',
+    }),
     NDVI_20220930: L.tileLayer.wms(geoserver, {
         layers: 'NDVI_20220930',
         transparent: 'true',
         format: 'image/png',
     }),
     what_is_here: L.tileLayer.wms(geoserver,{
-        layers: "whats_this",
+        layers: "whats_here",
         transparent: 'true',
         format: 'image/png',
     }),
